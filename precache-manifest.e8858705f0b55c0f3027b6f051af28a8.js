09b24ae4d02dadccb8d1c4d4ee4b56a2c582bb17
Leandro <leandro.frigerio@devugees.org>
2019-04-11 11:17:19 +0200

    Updates

---
+++ precache-manifest.e8858705f0b55c0f3027b6f051af28a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbda28235e32e845fe08ade3afa4666a",
+    "url": "/react-dashboard/static/media/reload.dbda2823.svg"
+  },
+  {
+    "revision": "582ccc7f229222f52ec6",
+    "url": "/react-dashboard/static/js/runtime~main.6bc13b0f.js"
+  },
+  {
+    "revision": "041922afd63e70554106",
+    "url": "/react-dashboard/static/js/main.d9fce1b9.chunk.js"
+  },
+  {
+    "revision": "716262ea4fcac6ad199f",
+    "url": "/react-dashboard/static/js/2.5deb269e.chunk.js"
+  },
+  {
+    "revision": "041922afd63e70554106",
+    "url": "/react-dashboard/static/css/main.340e32bb.chunk.css"
+  },
+  {
+    "revision": "9e241f7459972cffd6365b6f0f3bc200",
+    "url": "/react-dashboard/index.html"
+  }
+];
